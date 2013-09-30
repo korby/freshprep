b187f2b532446ec44bf334ff64a8a50adb26c1de
a.cianfarani <a.cianfarani@c2is.fr>
2013-09-30 09:50:13 +0200

    Corrected path displayed

--- tasks/help.php
+++ tasks/help.php
@@ -1,11 +1,11 @@
 <?php
 
-$currentWd = trim(shell_exec("pwd"));
+
 
 if ($handle = opendir($tasksDir)) {
   while (false !== ($entry = readdir($handle))) {
     if ($entry != "." && $entry != "..") {
-      echo $currentWd."/task ".str_replace(".php","",$entry)." help\n";
+      echo $rootDir."/task ".str_replace(".php","",$entry)." help\n";
     }
   }
 
